bd4ce826ce0cff78527375d6607e4738bee807fe
Maxim Tolkachev <maks.tolkachev.99@gmail.com>
2025-01-12 21:54:50 +0700

    UPDATE: add active menu tabs logic

--- resources/views/blocks/menu.blade.php
+++ resources/views/blocks/menu.blade.php
@@ -2,25 +2,28 @@
     <div class="container">
         <div class="collapse navbar-collapse" id="navbarMenu">
             <ul class="navbar-nav mx-auto">
+
                 <li class="nav-item">
-                    <a class="nav-link active" href="{{ route('home') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('article.list', 'research-articles') }}">Research articles</a>
+                    <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
                 </li>
 
-                <li class="nav-item dropdown">
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('blog/research-articles') ? 'active' : '' }}" href="{{ route('article.list', 'research-articles') }}">Research articles</a>
+                </li>
+
+                <li class="nav-item dropdown {{ Request::is('blog*') && !Request::is('blog/research-articles') ? 'active' : '' }}">
                     <a class="nav-link dropdown-toggle" href="{{ route('article.index') }}" id="dropdown05" aria-haspopup="true" aria-expanded="false">Articles</a>
                     <div class="dropdown-menu" aria-labelledby="dropdown05">
                         @foreach($list as $item)
                             <a class="dropdown-item" href="{{ route('article.list', $item->slug) }}">{{ $item->title }}</a>
                         @endforeach
                     </div>
-
                 </li>
-                <li class="nav-item">
+
+                <li class="nav-item {{ Request::is('about') ? 'active' : '' }}">
                     <a class="nav-link" href="{{ route('about') }}">About</a>
                 </li>
+
             </ul>
 
         </div>
